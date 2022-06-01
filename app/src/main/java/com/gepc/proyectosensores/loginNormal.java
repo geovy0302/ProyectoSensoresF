69249ce426785f994b15ed638ebcf8ad890eb0f2
geovy0302 <geovy032002@gmail.com>
2022-06-01 14:42:22 -0600

    Creación de pantalla Login y algunos archivos de estilos

---
+++ app/src/main/java/com/gepc/proyectosensores/loginNormal.java
@@ -0,0 +1,14 @@
+package com.gepc.proyectosensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginNormal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_normal);
+    }
+}
